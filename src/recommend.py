3b62eca9e7e6571cd88f29a32e440e4fe2a43d9f
Uday <udaybhaskarsimhadri01@gmail.com>
2025-07-06 00:35:53 +0530

    added project

--- src/recommend.py
+++ src/recommend.py
@@ -13,7 +13,7 @@ logging.basicConfig(
 )
 logging.info("🔁 Loading data...")
 try:
-    df = joblib.load('df_cleaned.pkl')
+    df = joblib.load('src/df_cleaned.pkl')
     cosine_sim = joblib.load('cosine_sim.pkl')
     logging.info("✅ Data loaded successfully.")
 except Exception as e:
